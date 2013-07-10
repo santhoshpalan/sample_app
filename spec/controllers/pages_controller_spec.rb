c9db28c3f38676f38c9b2428273f6ea31af57daf
Santhosh Palan <spalan@transmed.net>
2013-07-10 04:24:27 -0700

    Done with Static pages

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,19 +1,33 @@
 require 'spec_helper'
-
 describe PagesController do
 
-  describe "GET 'home'" do
-    it "returns http success" do
-      get 'home'
-      response.should be_success
-    end
-  end
+render_views
+describe "GET 'home'" do
+it "should be successful" do
+get 'home'
+response.should be_success
+end
+end
+describe "GET 'contact'" do
+it "should be successful" do
+get 'contact'
+response.should be_success
+end
+end
 
-  describe "GET 'contact'" do
-    it "returns http success" do
-      get 'contact'
-      response.should be_success
-    end
-  end
+describe "GET 'about'" do
+
+it "should be successful" do
+get 'about'
+response.should be_success
+end
+
+it "should have right title" do
+get 'about'
+response.should have_selector("title", :content=> "ROR sample App About")
+end
+
+end
+
 
 end
